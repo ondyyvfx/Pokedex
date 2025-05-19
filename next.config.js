e4fcc75985ca07a1f581baa47c742d9f56eb24d3
ondyy <ondyyvfx@gmail.com>
2025-05-19 18:04:06 +0500

    next config remotePatterns

---
+++ next.config.js
@@ -0,0 +1,16 @@
+// next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
